0cb2b9cbed58f1868b4ee8bce7bf6ce07dcaced3
Rensage <122806717+Ransege@users.noreply.github.com>
2025-12-15 09:26:43 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -116,10 +116,10 @@ def handle_callbacks(bot, call, OWNER_ID):
         elif call.data == 'show_updates':
             bot.answer_callback_query(call.id)
             bot.send_message(call.message.chat.id,
-                "Последние обновления (14.12.2025):\n"
+                "Последние обновления (11.12.2025):\n"
                 "•  Кнопка «Выход из чата» при запущенном AI-CHAT\n"
-                "•  Кнопка «Лицензия и авторство» в Главном меню",
-                "• В Mini App добавлена мини игра",
+                "• Кнопка «Лицензия и авторство» в Главном меню\n",
+                "• В Mini App добавлена мини игра\n",
                 "• В Mini App добавлен актуальный прайс-лист",
                 reply_markup=get_return_markup())
 
